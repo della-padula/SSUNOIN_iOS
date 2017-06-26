1c5d8f6c23dec6d8b3b6c90ff63848fb65e25d8d
김태인 <terry@gimtaein-ui-MacBook-Pro.local>
2017-06-26 21:14:26 +0900

    StoryBoard 화면 구성 틀 구현 완료

--- SSUNOIN_iOS/ViewController.swift
+++ SSUNOIN_iOS/ViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var MajorTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
