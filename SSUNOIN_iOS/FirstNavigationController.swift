f17cb8880181920f7f6d4c4b7de56b61f1b1ff73
김태인 <terry@gimtaein-ui-MacBook-Pro.local>
2017-06-26 21:00:45 +0900

    Storyboard 화면 추가 완료

---
+++ SSUNOIN_iOS/FirstNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  FirstNavigationController.swift
+//  SSUNOIN_iOS
+//
+//  Created by 김태인 on 2017. 6. 26..
+//  Copyright © 2017년 Personal. All rights reserved.
+//
+
+import UIKit
+
+class FirstNavigationController : UINavigationController {
+    
+}
---
+++ SSUNOIN_iOS/Major.swift
@@ -0,0 +1,15 @@
+//
+//  Major.swift
+//  SSUNOIN_iOS
+//
+//  Created by 김태인 on 2017. 6. 26..
+//  Copyright © 2017년 Personal. All rights reserved.
+//
+
+import UIKit
+
+class Major : UITableViewCell {
+    @IBOutlet weak var MajorName: UILabel!
+    @IBOutlet weak var MajorEngName: UILabel!
+    
+}
